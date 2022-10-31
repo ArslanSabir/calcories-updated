2195fa3d934e6dc5c248346b2294f57b6472d662
Arslan Sabir <arslansabir141@gmail.com>
2022-10-31 06:45:36 +0500

    working

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,22 +81,28 @@ AUTH_USER_MODEL = 'users.User'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'mssql',
-#         'NAME': 'database name',
-#         'USER': 'username',
-#         'PASSWORD': 'password',
-#         'HOST': 'host name',
-#         'PORT': port,
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'cal',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'ARSLAN',
+        'PORT': '',
+        #'TrustedConnection': 'True',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
